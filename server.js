90b5f13345fffd64febe6d0d359e11b78cd5ef5f
karanpalkurukshetra-source <karanpalkurukshetra@gmail.com>
2026-04-26 19:53:52 +0530

    server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const razorpay = new Razorpay({
 });
 
 // 🔥 Firebase Admin
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
